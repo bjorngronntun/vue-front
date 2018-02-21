eeecb1c7c7f97a86178620231f00c2f2c2fc1585
bjorngronntun <bjorngronntun@gmail.com>
2018-02-21 19:49:23 +0100

    Added one graph, bad organization

--- app.py
+++ app.py
@@ -4,7 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return render_template('front.html')
+    return 'Hello world'
+    
+@app.route('/graph1')
+def home1():
+    return render_template('front1.html')
+
+@app.route('/graph2')
+def home2():
+    return render_template('front2.html')
 
 if __name__ == '__main__':
     app.run(port=5050, debug=True)
